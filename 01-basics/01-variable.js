59555ede424ac99bd7b442fb76de6f40a83e1b13
Jugal D. <sutharjugalk@gmail.com>
2024-02-26 18:57:03 +0000

    variable file commit

---
+++ 01-basics/01-variable.js
@@ -0,0 +1,8 @@
+const accountId = 144553;
+let actEmail = "sutharjugalk@gmail.com";
+var accoutPwd = "123A123";
+actCity = "Anupgarh";
+let accState;
+// accountId = 1;
+console.log(accountId);
+console.table([accountId, actEmail, actCity, accState]);
