6d7259466d9d785493169fe8e522b469b5db4b0b
Jugal D. <sutharjugalk@gmail.com>
2024-02-27 13:54:34 +0000

    compelete file

---
+++ 01-basics/converstation.js
@@ -0,0 +1,10 @@
+let score = "33ava";
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber = Number(score);
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+
+let isLoggedIn = 6;
+let boolienIsLoggedIn = Boolean(isLoggedIn);
+console.log(boolienIsLoggedIn);
