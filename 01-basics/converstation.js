28ef7ce7193af1b788d75c08e18127edd8dde427
Jugal D. <sutharjugalk@gmail.com>
2024-02-28 17:49:14 +0000

    conversion end

--- 01-basics/converstation.js
+++ 01-basics/converstation.js
@@ -11,4 +11,14 @@ console.log(boolienIsLoggedIn);
 // operations
 let one = 1;
 let two = 3;
-console.log(one+two);
+// console.log(one+two);
+// console.log(one-two);
+// console.log(one*two);
+// console.log(one/two);
+// console.log(one**two);
+// console.log(one%two);
+// console.log(one+two);
+let srt1 = "Java";
+let srt2 = "Scricpt";
+let comboStr = srt1 +"  "+ srt2;
+console.log(comboStr);
