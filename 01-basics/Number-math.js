10d5be9a33d8624c9a1229b1d4cf62bb15a1b3d6
Jugal D. <sutharjugalk@gmail.com>
2024-03-01 04:32:27 +0000

    learn math function

--- 01-basics/Number-math.js
+++ 01-basics/Number-math.js
@@ -1,1 +1,14 @@
-    
+const Numb = 400.46465
+const newNum = new Number(300);
+// console.log(Numb);
+// console.log(newNum);
+// console.log(Numb.toFixed(2));
+// console.log(Numb.toPrecision(4));
+// const numbr = 10000000;
+// console.log(numbr.toLocaleString()); // it represents standered number format
+// console.log(numbr.toLocaleString('en-IN')); // it represents indian number format
+//+++++++++++++++++++++++ Maths++++++++++++++++++++++++
+// console.log(Math.floor(5.5));
+// console.log(Math.ceil(5.5));
+// console.log(Math.round(5.5));
+console.log(Math.round(Math.random(5.5)*10));
