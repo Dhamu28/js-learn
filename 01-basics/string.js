4a65bcd6709064856374450b3f041debd554959f
Jugal D. <sutharjugalk@gmail.com>
2024-03-01 04:08:20 +0000

    update string file

--- 01-basics/string.js
+++ 01-basics/string.js
@@ -2,3 +2,9 @@ const str1 = "Dhamu";
 const str2 = "Jugal";
 
 console.log(`Full name of ${str2} is ${str2} ${str1}`);
+const newStr = new String('   Jugal Dhamu  ');
+console.log(newStr);
+console.log(newStr.length);
+console.log(newStr.toUpperCase());
+console.log(newStr.trim());
+console.log(newStr.length);
