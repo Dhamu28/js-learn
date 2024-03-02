38c16e998a2986a9338363c7876d064d62ff96c0
Jugal D. <sutharjugalk@gmail.com>
2024-03-02 02:32:24 +0000

    02-basics folder created

---
+++ 02-basics/arrays.js
@@ -0,0 +1,16 @@
+// array
+const myArray = [12, 3, 6, 56, 14, 55];
+console.log(myArray[3]);
+const myHeroes =["Apple", "Banana", "Mengo"];
+console.log(myHeroes);
+myHeroes.push("book");
+console.log(myArray.length);
+console.log(myHeroes);
+// remove last value
+myHeroes.pop();
+
+// console.log(myHeroes);
+
+myArray.shift(5);
+console.log(myArray);
+console.log(myHeroes);
