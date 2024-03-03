35d11f6f818de6b325855e77f8f536647d4b074f
Jugal D. <sutharjugalk@gmail.com>
2024-03-03 19:18:31 +0000

    object lession done

---
+++ 02-basics/object.js
@@ -0,0 +1,17 @@
+// singleton objected call constrator
+// Object.create (constractor)
+// object literals
+const symblAsKey = Symbol("key")
+const jsUser = {
+Name : "Jugal",
+Cast : "Dhamu",
+[symblAsKey] : "307191",
+Age : "32",
+location : "aph"
+}
+console.log(jsUser);
+console.log(jsUser.Cast);
+console.log(jsUser["Cast"]);
+console.log(jsUser[symblAsKey]);
+// Object.freeze("Name"); array value freeze that not to be change
+jsUser.name = "Jugal Kishore";
