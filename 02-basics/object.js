9d0d9fae40f5a95d0b5ab084c70c1ecd51f6f4fb
Jugal D. <sutharjugalk@gmail.com>
2024-03-05 01:34:29 +0000

    object file updated

--- 02-basics/object.js
+++ 02-basics/object.js
@@ -9,9 +9,17 @@ Cast : "Dhamu",
 Age : "32",
 location : "aph"
 }
-console.log(jsUser);
-console.log(jsUser.Cast);
-console.log(jsUser["Cast"]);
-console.log(jsUser[symblAsKey]);
+// console.log(jsUser);
+// console.log(jsUser.Cast);
+// console.log(jsUser["Cast"]);
+// console.log(jsUser[symblAsKey]);
 // Object.freeze("Name"); array value freeze that not to be change
 jsUser.name = "Jugal Kishore";
+
+// Object class second
+// Singleton object
+// const tinderUser = new Object();
+// Non Single tone object
+const tinderUser= {};
+
+console.log(tinderUser);
