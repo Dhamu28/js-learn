f0600c71142382e4d9b99e560a259b0260f03b2a
Jugal D. <sutharjugalk@gmail.com>
2024-03-05 02:19:06 +0000

    object file update with join two obj

--- 02-basics/object.js
+++ 02-basics/object.js
@@ -19,7 +19,31 @@ jsUser.name = "Jugal Kishore";
 // Object class second
 // Singleton object
 // const tinderUser = new Object();
-// Non Single tone object
+// Constractor object
 const tinderUser= {};
+tinderUser.id = "225"
+tinderUser.name = "Jugal"
+tinderUser.age = "32"
+tinderUser.isLogedIn = true
 
 console.log(tinderUser);
+
+const regularUser = {
+    email : "avc@com",
+    fullName : {
+        firstName : "Jivan",
+        lastName : "Suthar"
+    },
+    hobby : ["playing", "Singing", "Listening"]
+};
+console.log(regularUser.fullName.firstName);
+console.log(regularUser.hobby[1]);
+const obj1 = {1 : "One", 2 : "two", 3 : "threee"};
+const obj2 = {11 : "EleOne", 12 : "twelo", 13 : "threteen"};
+console.log(obj1);
+console.log(obj2);
+// merge objects 
+const obj3 = Object.assign({}, obj1, obj2);
+console.log(obj3);
+console.log(obj1);
+console.log(obj2);
