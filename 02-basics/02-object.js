dab9cd559dfb721a25d90b9b2217bdce3966730b
Jugal D. <sutharjugalk@gmail.com>
2024-03-05 19:08:11 +0000

    read api and json

---
+++ 02-basics/02-object.js
@@ -0,0 +1,20 @@
+//destructure  a object
+const course = {
+    courseName : "Js in hindi",
+    courseFee : 9999,
+    courseInst : "Hitesh"
+};
+const {courseName} = course;
+console.log(courseFee);
+// api usually is a json data 
+// {
+//     "courseName" : "Js in hindi",
+//     "courseFee" : 9999,
+//     "courseInst" : "Hitesh"
+// }
+// or it should be in array with object 
+[
+    {},
+    {},
+    {}
+]
