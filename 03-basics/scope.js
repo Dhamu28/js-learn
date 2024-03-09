5a436cab9283a57447d41607e380a90a6bee3087
Jugal D. <sutharjugalk@gmail.com>
2024-03-09 14:45:46 +0000

    read more about var scope

--- 03-basics/scope.js
+++ 03-basics/scope.js
@@ -4,6 +4,24 @@ function count()
     let a = 10;
     let b = 40;
 console.log(a);
+// console.log(num);
+    function counter(){
+        let num = 25;
+        console.log(num);
+        console.log(b);
+    }
+    counter();
 }
 count();
 console.log(a);
+addOne();// it can be use because it's defined
+
+function addOne(number){
+    return number+1;
+}
+
+addTwo();// this is not defiend
+
+const addTwo = function(numb){
+    return numb+2;
+}
