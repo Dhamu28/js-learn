2b71a46ec4fdccd39f9f338acf88519caf030c32
Jugal D. <sutharjugalk@gmail.com>
2024-03-25 17:45:44 +0000

    add arrow function file

---
+++ 03-basics/arrow_function.js
@@ -0,0 +1,9 @@
+const user = {
+    username : "Jugal",
+    price : 009,
+    welComeMsj : function (){
+        console.log(`${this.username}, Welcome to Website`);
+    }
+}
+user.welComeMsj();
+console.log(user.welComeMsj());
